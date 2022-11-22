5b7b7acfed7da2e1bad35d79954408591622a208
Park-Kwanwoo <pgw4712@icloud.com>
2022-11-22 21:13:49 +0900

    카카오 회원가입 추가

--- src/views/user/JoinView.vue
+++ src/views/user/JoinView.vue
@@ -1,58 +1,54 @@
 <template>
-  <div class="mt-2">
-    <h3>아이디</h3>
-    <p id="email"> {{ errorResponse.email }} </p>
-    <el-input v-model="email" placeholder="이메일을 입력해주세요."/>
-  </div>
+  <div class="container">
+    <div class="mt-2">
+      <h3>이메일</h3>
+      <p id="email"> {{ errorResponse.email }} </p>
+      <el-input v-model="email" placeholder="이메일을 입력해주세요."/>
+    </div>
 
-  <div class="mt-2">
-    <h3>비밀번호</h3>
-    <p id="password"> {{ errorResponse.password }} </p>
-    <el-input v-model="password" type="password" rows="15" placeholder="숫자, 문자, 특수문자 포함 8~15자리 이내로 입력해주세요."/>
-  </div>
+    <div class="mt-2">
+      <h3>비밀번호</h3>
+      <p id="password"> {{ errorResponse.password }} </p>
+      <el-input v-model="password"
+                type="password"
+                rows="15"
+                placeholder="숫자, 문자, 특수문자 포함 8~15자리 이내로 입력해주세요."
+      />
+    </div>
 
+    <div class="mt-2">
+      <h3>닉네임</h3>
+      <p id="name">{{ errorResponse.nickname }}</p>
+      <el-input v-model="nickname" rows="15" placeholder="닉네임을 입력해주세요."></el-input>
+    </div>
+    <div class="mt-2 float-end">
+      <el-button type="primary" @click="join()">회원가입</el-button>
+    </div>
 
-  <div class="mt-2">
-    <h3>전화번호</h3>
-    <p id="phone"> {{ errorResponse.phone }} </p>
-    <el-input v-model="phone" rows="15"
-              maxlength="13"
-              placeholder="숫자만 입력해주세요."
-              :formatter='(phone) => `${phone}`.replace(/[^0-9]/g, "")
-    .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3").replace(/(\-{1,2})$/g, "")'
-    >
-    </el-input>
-  </div>
+    <div class="mt-3">
+      <el-button link @click="getKakaoCode"
+         type="primary">
+       카카오 회원가입
+      </el-button>
+    </div>
 
-
-  <div class="mt-2">
-    <h3>닉네임</h3>
-    <p id="name">{{ errorResponse.nickname }}</p>
-    <el-input v-model="nickname" rows="15" placeholder="닉네임을 입력해주세요."></el-input>
-  </div>
-
-  <div class="mt-2">
-    <el-button type="primary" @click="join()">회원가입</el-button>
   </div>
 </template>
 
 <script setup lang="ts">
 
-import {ref} from "vue";
+import {computed, ref, watch} from "vue";
 import axios from "axios";
 import {useRouter} from "vue-router";
 
 const router = useRouter();
 const email = ref('');
-const phone = ref('');
 const nickname = ref('');
 const password = ref('');
 
 const errorResponse = ref({
   email: '',
-  password: '',
-  nickname: '',
-  phone: ''
+  nickname: ''
 })
 
 const join = function () {
@@ -61,7 +57,6 @@ const join = function () {
     email: email.value,
     password: password.value,
     nickname: nickname.value,
-    phone: phone.value
   })
       .then((r) => {
         if (r.status == 200) {
@@ -69,15 +64,68 @@ const join = function () {
         }
       })
       .catch((e) => {
-        errorResponse.value = {email: '', nickname: '', phone: '', password: ''};
+        errorResponse.value = {email: '', nickname: ''};
         errorResponse.value = e.response.data.validation;
       })
 }
+
+const getKakaoCode = function () {
+  location.href = `https://kauth.kakao.com/oauth/authorize?redirect_uri=${import.meta.env.VITE_KAKAO_JOIN_REDIRECT_URI}&response_type=code&client_id=${import.meta.env.VITE_KAKAO_LOGIN_CLIENT_ID}`;
+};
+
+watch(async () => {
+  const getAccesstoken = async (authorizationCode: any) => {
+
+    axios.get(`https://kauth.kakao.com/oauth/token?grant_type=authorization_code&code=${authorizationCode}&client_id=${import.meta.env.VITE_KAKAO_LOGIN_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_KAKAO_JOIN_REDIRECT_URI}&client_secret=${import.meta.env.VITE_KAKAO_LOGIN_CLIENT_SECRET}`)
+        .then(res => {
+          axios.post("https://kapi.kakao.com/v2/user/me", null, {
+            headers: {
+              "Authorization": "Bearer " + res.data.access_token,
+              "Content-type": "application/x-www-form-urlencoded;charset=utf-8"
+            }
+          })
+              .then(res => {
+                axios.post('/api/members/join', {
+                  email: res.data.kakao_account.email,
+                  nickname: res.data.properties.nickname,
+                  auth: "kakao"
+                })
+                    .then(res => {
+                      if (res.status == 200) {
+                        alert("회원 가입에 성공하였습니다!");
+                        router.push({name: 'home'});
+                      }
+                    })
+                    .catch(err => {
+                      alert(err.response.data.message);
+                    })
+              })
+              .catch(err => {
+                alert("오류가 있습니다. 다시 시도해주세요");
+              })
+        })
+        .catch(err => {
+          alert(err);
+        })
+  }
+
+  const authorizationCode = new URL(window.location.href).searchParams.get("code");
+  if (authorizationCode) {
+    await getAccesstoken(authorizationCode);
+  }
+}, () => {
+})
+
+
 </script>
 
 <style scoped>
 p {
   color: red;
   font-size: 0.8rem;
+}
+
+.container {
+  width: 500px;
 }
 </style>
