3dd990d8bf065c6e109eafa7747a2e9e673c84ae
Park-Kwanwoo <pgw4712@icloud.com>
2022-11-22 18:59:44 +0900

    환경변수 키 추가

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,9 @@
 interface ImportMetaEnv {
     VITE_MAP_KEY: string
     VITE_LOCAL_KEY: string
+    VITE_KAKAO_LOGIN_CLIENT_ID: string
+    VITE_KAKAO_LOGIN_CLIENT_SECRET: string
+    VITE_KAKAO_REDIRECT_URI: string
 }
 
 interface ImportMeta {
